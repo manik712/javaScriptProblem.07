da29d210adec665a92e2e96b24d1fc7fbbeb5b90
manik712 <ranineela71@gmail.com>
2024-02-21 11:24:24 +0600

    first commit

--- problem.js
+++ problem.js
@@ -1,26 +1,35 @@
-function compare(a, b) {
-  if (a == b) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-const result = compare(15, "15");
-console.log(result);
-
-//  function compare(a, b) {
-//   if (a.toString() === b) {
-//   return true;
+// function compare(a, b) {
+//   if (a == b) {
+//     return true;
 //   } else {
-//   return false;
+//     return false;
 //   }
-//  }
-//  const result1 = compare(25, 25);
-//  console.log(result1);
+// }
 
-//  console.log("123" + 123);
+// const result = compare(15, "15");
+// console.log(result);
 
-//  let a="hi"
-//  let b="there"
-//  console.log(a+b)
+ function compare(a, b) {
+  if (a.toString() === b) {
+  return true;
+  } else {
+  return false;
+  }
+ }
+ const result1 = compare(25, 25);
+ console.log(result1);
+
+ console.log("123" + 123);
+
+
+
+
+
+
+
+
+
+ 
+ let a="hi"
+ let b="there"
+ console.log(a+b)
