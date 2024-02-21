2a7543c2e0709683a9e0601487c8013aa55456b3
manik712 <ranineela71@gmail.com>
2024-02-21 11:25:47 +0600

    added anothr problem

--- problem.js
+++ problem.js
@@ -9,27 +9,18 @@
 // const result = compare(15, "15");
 // console.log(result);
 
- function compare(a, b) {
+function compare(a, b) {
   if (a.toString() === b) {
-  return true;
+    return true;
   } else {
-  return false;
+    return false;
   }
- }
- const result1 = compare(25, 25);
- console.log(result1);
+}
+const result1 = compare(25, 25);
+console.log(result1);
 
- console.log("123" + 123);
+console.log("123" + 123);
 
-
-
-
-
-
-
-
-
- 
- let a="hi"
- let b="there"
- console.log(a+b)
+let a = "hi";
+let b = "there";
+console.log(a + b);
